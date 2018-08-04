c7dfc1871d1f825d6f3fe855a75b56d8eaf25c0a
David Stainton <dstainton415@gmail.com>
2018-08-04 20:31:59 +0100

    pyecho: allocate random unix domain socket

--- echo-python/plugin.py
+++ echo-python/plugin.py
@@ -3,13 +3,13 @@
 from concurrent import futures
 import sys
 import time
+import random
+import string
 
 import grpc
 
 import kaetzchen_pb2
 import kaetzchen_pb2_grpc
-
-
 
 
 class EchoServicer(kaetzchen_pb2_grpc.KaetzchenServicer):
@@ -22,17 +22,23 @@ class EchoServicer(kaetzchen_pb2_grpc.KaetzchenServicer):
 
 
 def serve():
+    rand = ''.join(random.choice(string.digits) for _ in range(10))
+    socket = "/tmp/pyecho_plugin_%s.sock" % rand
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     kaetzchen_pb2_grpc.add_KaetzchenServicer_to_server(
         EchoServicer(), server)
-    server.add_insecure_port('unix:/home/user/tmp/echo.sock')
+    server.add_insecure_port(socket)
     server.start()
+
+    # Output information
+    print("1|1|unix|%s|grpc" % socket)
+    sys.stdout.flush()
+
     try:
         while True:
             time.sleep(60 * 5)
     except KeyboardInterrupt:
         server.stop(0)
-
 
 if __name__ == '__main__':
     serve()
