af5471ae9a81eee22ef7348a01c0a7c9c607e1f9
David Stainton <dstainton415@gmail.com>
2020-07-26 02:24:20 +0000

    pubsub: echo, fix copyright and comment

--- pubsub_plugins/echo-go/main.go
+++ pubsub_plugins/echo-go/main.go
@@ -1,5 +1,5 @@
-// main.go - echo service using cbor plugin system
-// Copyright (C) 2018  David Stainton.
+// main.go - test echo service using pubsub plugin system
+// Copyright (C) 2020  David Stainton.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
